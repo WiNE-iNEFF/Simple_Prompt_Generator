208c1c5b0536f404525934e0f65c226461db2abe
Artsem Holub <41611046+WiNE-iNEFF@users.noreply.github.com>
2023-02-18 23:12:44 +0200

    Delete generator.py

--- generator.py
+++
@@ -1,9 +0,0 @@
-import random
-prompt = open('prompttext.txt').read().splitlines()
-vocab = len(prompt)
-generated = []
-num_word = 13
-while len(sorted(set(generated), key=lambda d: generated.index(d))) < num_word:
-	rand = random.randint(0, vocab)
-	generated.append(prompt[rand-1])
-print(' '.join(sorted(set(generated), key=lambda d: generated.index(d))))
