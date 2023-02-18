292abb8bfae2aa312029b8967b12bcfbf7649f7e
Artsem Holub <41611046+WiNE-iNEFF@users.noreply.github.com>
2023-02-18 23:12:22 +0200

    Delete gen02.py

--- gen02.py
+++
@@ -1,9 +0,0 @@
-import random
-prompt = open('prompttext2.txt', encoding='utf-8').read().splitlines()
-vocab = len(prompt)
-generated = []
-num_word = 13
-while len(sorted(set(generated), key=lambda d: generated.index(d))) < num_word:
-	rand = random.randint(0, vocab)
-	generated.append(prompt[rand])
-print(' '.join(sorted(set(generated), key=lambda d: generated.index(d))))
