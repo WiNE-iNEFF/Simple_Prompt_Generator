9b34d1afb9aabc318ea1dd8e5b37067d12eaf8ea
Artsem Holub <41611046+WiNE-iNEFF@users.noreply.github.com>
2023-02-18 23:22:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 import random
 
-def prompts(prompts="pr1.txt", num=10, author=2):
+def prompts(prompts="pr1.txt", num=10, artists=2):
 	if prompts == "pr1.txt":
 		prompt = open('pr1.txt', encoding='utf-8').read().splitlines()
 	elif prompts == "pr2.txt":
@@ -9,13 +9,13 @@ def prompts(prompts="pr1.txt", num=10, author=2):
 
 	vocab = len(prompt)
 	generated = []
-	author_num = 0
+	artists_num = 0
 	num_word = num
 
 	while len(sorted(set(generated), key=lambda d: generated.index(d))) < num_word:
 		rand = random.randint(0, vocab)
-		if prompt[rand-1].startswith('art by') and author_num < author:
-			author_num +=1 
+		if prompt[rand-1].startswith('art by') and artists_num < artists:
+			artists_num +=1 
 			generated.append(prompt[rand-1])
 		elif not prompt[rand-1].startswith('art by'):
 			generated.append(prompt[rand-1])
@@ -30,10 +30,10 @@ class MainArg():
 		parser = argparse.ArgumentParser()
 		parser.add_argument('-p', '--prompts', help='Prompt version', required=True)
 		parser.add_argument('-n', '--num', help='Num of prompts (Standart: 10, MAX: 20)', required=False)
-		parser.add_argument('-a', '--author', help='How many author use in prompt (Standart: 2)', required=False)
+		parser.add_argument('-a', '--artists', help='How many artists use in prompt (Standart: 2)', required=False)
 
 		args = parser.parse_args()
-		prompts(args.prompts, int(args.num), int(args.author))
+		prompts(args.prompts, int(args.num), int(args.artists))
 
 if __name__ == "__main__":
 	main = MainArg()
